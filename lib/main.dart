f9cbd4aaac81fbee0835ac6127b2baea57fb0998
bhgong <bhgong512@gmail.com>
2023-08-08 23:23:45 +0900

    #4.2 Auth Button

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tiktok_clone/constants/gaps.dart';
-import 'package:tiktok_clone/constants/sizes.dart';
-import 'package:tiktok_clone/sign_up/sign_up_screen.dart';
+import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
 
 void main() {
   runApp(const TikTokApp());
@@ -9,8 +7,6 @@ void main() {
 
 class TikTokApp extends StatelessWidget {
   const TikTokApp({super.key});
-
-  // 이걸로 해서 아이패드 있으면 원격으로 코딩할 수 있겠어.. 야스..
 
   // This widget is the root of your application.
   @override
